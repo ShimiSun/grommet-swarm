be226f2afe9162dd6a9d246d3fc25412e2611f78
shimisun <shimrit.yacobi@gmail.com>
2019-05-14 17:08:34 -0600

    add descriptive text content

---
+++ src/data/descriptiveLearning.js
@@ -0,0 +1,4 @@
+export const descriptiveLearning = [
+  'Collaborative model training occurs at Edge devices and at the Data Center. Learned parameters are merged at the Edge.',
+  'Data is contained at the Edge to ensure privacy. Leaders are dynamically elected for merging parameters.',
+];
