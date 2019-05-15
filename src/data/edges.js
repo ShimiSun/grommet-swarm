e8843dabeb97f0f8cc553cedbdaed793a9f0e66f
shimisun <shimrit.yacobi@gmail.com>
2019-05-15 14:53:19 -0600

    adding swarm data file

--- src/data/edges.js
+++ src/data/edges.js
@@ -1,10 +1,16 @@
 export const edges = [
-  { name: 'Edge 001', accuracy: '40%', time: '8.4s', utilization: '43%' },
-  { name: 'Edge 002', accuracy: '50%', time: '8.4s', utilization: '43%' },
+  { name: 'Berlin 001', accuracy: '40%', time: '8.4s', utilization: '43%' },
+  { name: 'Mumbai 001', accuracy: '50%', time: '8.4s', utilization: '43%' },
   {
-    name: 'Las Vegas DC 002',
+    name: 'Las Vegas DC 001',
     accuracy: '60%',
     time: '8.4s',
     utilization: '57%',
   },
 ];
+
+export const swarmEdge = {
+  name: 'Swarm 001',
+  accuracy: '80%',
+  time: '8.4 seconds',
+};
