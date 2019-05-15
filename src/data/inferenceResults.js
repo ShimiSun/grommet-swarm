d1ae6c3c21010dd140b8d6ce03e47de4dd8a78b9
shimisun <shimrit.yacobi@gmail.com>
2019-05-15 08:58:13 -0600

    adding data samples

---
+++ src/data/inferenceResults.js
@@ -0,0 +1,9 @@
+export const inferenceResults = [
+  { name: 'Discover Node 001', car: 80, truck: 40, ship: 10 },
+  { name: 'Discover Node 002', car: 80, truck: 40, ship: 10 },
+  { name: 'Discover Node 003', car: 80, truck: 40, ship: 10 },
+];
+
+export const inferenceResultsSwarm = [
+  { name: 'SWARM 001', car: 80, truck: 60, ship: 10 },
+];
