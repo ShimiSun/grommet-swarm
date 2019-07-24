e792fceb93d54ea7f8bd1153fded6e6776f0831f
shimisun <shimrit.yacobi@gmail.com>
2019-07-24 13:09:41 -0600

    add responsive

--- src/components/Inference/InferenceInfoEmpty.js
+++ src/components/Inference/InferenceInfoEmpty.js
@@ -1,29 +1,40 @@
 import React from 'react';
 
 import { Cube, Inherit } from 'grommet-icons';
-import { Box, Text } from 'grommet';
+import { Box, ResponsiveContext, Text } from 'grommet';
 import { inferenceResults } from '../../data/inferenceResults';
 
 export const cubes = inferenceResults.map(edge => (
-  <Box key={edge.name}>
-    <Box gap="medium" direction="row">
-      <Cube color="grommet" size="xlarge" />
-      <Text alignSelf="center"> {edge.name}</Text>
-    </Box>
-  </Box>
+  <ResponsiveContext.Consumer key={edge.name}>
+    {size => (
+      <Box>
+        <Box gap="medium" direction="row">
+          <Cube color="grommet" size={size === 'small' ? 'large' : 'xlarge'} />
+          <Text alignSelf="center"> {edge.name}</Text>
+        </Box>
+      </Box>
+    )}
+  </ResponsiveContext.Consumer>
 ));
 
 export const InferenceInfoEmpty = () => (
-  <Box direction="row" margin="large" justify="between" align="center">
-    <Box gap="medium">{cubes}</Box>
-    <Box
-      alignSelf="center"
-      pad={{ horizontal: 'xlarge' }}
-      direction="row"
-      gap="medium"
-    >
-      <Inherit color="accent-3" size="xlarge" />
-      <Text alignSelf="center"> Swarm 001</Text>
-    </Box>
-  </Box>
+  <ResponsiveContext.Consumer>
+    {size => (
+      <Box direction="row" margin="large" justify="between" align="center">
+        <Box gap="medium">{cubes}</Box>
+        <Box
+          alignSelf="center"
+          pad={{ horizontal: 'xlarge' }}
+          direction="row"
+          gap="medium"
+        >
+          <Inherit
+            color="accent-3"
+            size={size === 'small' ? 'large' : 'xlarge'}
+          />
+          <Text alignSelf="center"> Swarm 001</Text>
+        </Box>
+      </Box>
+    )}
+  </ResponsiveContext.Consumer>
 );
